93214b5c6e68fe2794b831f0f779994f25e59fe1
infotrex <infotrex@yahoo.com>
2022-01-11 09:48:21 +0800

    Delete NFT directory
    unutilise

--- NFT/__init__.py
+++
@@ -1,7 +0,0 @@
-class NFT:
-
-    def __init__(self, name, description, metadata, file):
-        self.name = name
-        self.description = description
-        self.metadata = metadata
-        self.file = file
